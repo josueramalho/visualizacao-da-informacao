16e47e94aec464d96f333e8d45c2f30d289fc789
Josué Ramalho <josuermlh@gmail.com>
2021-05-15 18:58:28 -0300

    Adptação para novo dataset

--- grafico1.py
+++ grafico1.py
@@ -2,16 +2,18 @@ import plotly.express as px
 import pandas as pd
 
 # Importando arquivo CSV que será usado
-dataset = pd.read_csv('https://raw.githubusercontent.com/josueramalho/visualizacao-da-informacao/master/dataset_1.csv')
-index = pd.Series(dataset['causa'])
-
-# Separando dados para melhor visualização e tratamento
-y_data = dataset['causa']
-x_data= dataset['total_global']
-
-# Criando o gráfico
-fig = px.bar(index, x_data, y_data, orientation='h', labels={'y': 'Causa', 'x': 'Mortes (em milhões)'},
-            title='10 Principais causas de morte no mundo em 2017 (Fonte: OMS)', text=x_data)
-            
-#Exibindo o gráfico
+dataset = pd.read_csv('https://github.com/josueramalho/visualizacao-da-informacao/raw/master/dataset.csv')
+dataset = pd.DataFrame(dataset)
+# Selecionando e tratando apenas os dados que serão usados no gráfico 1
+deaths_df = dataset.loc[dataset['Year'] == 2017]
+world_deaths = deaths_df.loc[deaths_df['Entity'] == 'World']
+world_deaths = world_deaths.drop(['Entity', 'Code', 'Year'], axis=1)
+world_deaths = world_deaths.sort_values(by = 6573, axis=1, ascending=False)
+# Criando novo dataset com dados tratados
+x_data = list(world_deaths.loc[6573])
+y_data = list(world_deaths.columns)
+# Criando gráfico
+fig = px.bar(x=x_data, y=y_data, orientation='h', labels={'y': 'Causa', 'x': 'Mortes (em milhões)', 'text':'Float'},
+            title='Principais causas de morte no mundo em 2017 (Fonte: OMS)', text=x_data, range_y=[0, 10])
+# Renderizando gráfico
 fig.show()
