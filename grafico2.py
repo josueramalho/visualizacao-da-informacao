cc688c02125c5110061e38ad2dbd09c9c94ceb5d
Josué Ramalho <josuermlh@gmail.com>
2021-05-16 07:27:58 -0300

    Código do gráfico 2

---
+++ grafico2.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+#Importando dataset base
+file_url = '/home/josue/Documents/cc/information_visualization/projeto/dataset.csv'
+dataset = pd.read_csv(file_url)
+dataset = pd.DataFrame(dataset)
+#Tratando dataset e selecionando os dados que serão usados
+topic = 'Deaths - Road injuries - Sex: Both - Age: All Ages (Number)'
+deaths_df = dataset.loc[dataset['Entity'] == 'World', ['Entity','Year', topic]]
+numbers = list(pd.Series(deaths_df[topic]))
+years = list(pd.Series(deaths_df.Year))
+#Criando uma list para tornar mais simples e visualmente amigável o plot
+deaths_df = [years, numbers]
+# Criando o gráfico
+plt.plot(years, numbers)
+plt.title('Mortes no trânsito de 1990 a 2017, segundo a OMS (Organização Mundial da Saúde)')
+plt.ylabel('Mortes (em milhões)')
+# Renderizando
+plt.show()
