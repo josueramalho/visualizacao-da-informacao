ce156cd843c0edf308835d12b29a7cf881db386d
Josué Ramalho <josuermlh@gmail.com>
2021-05-18 13:11:40 -0300

    Atualizando comentarios e texto

--- grafico3.py
+++ grafico3.py
@@ -1,18 +1,18 @@
 import pandas as pd
 from plotly import graph_objects as go
-#Importando dataset base
+# Importando dataset base
 file_url = 'https://github.com/josueramalho/visualizacao-da-informacao/raw/master/src/dataset.csv'
 dataset = pd.read_csv(file_url)
 dataset = pd.DataFrame(dataset)
-#Tratando dataset e selecionando os dados que serão usados
+# Tratando dataset e selecionando os dados que serão usados
 topic = 'Deaths - Road injuries - Sex: Both - Age: All Ages (Number)'
 deaths_df = dataset.loc[dataset['Year'] == 2017, ['Entity','Year', topic]]
 numbers = list(pd.Series(deaths_df[topic]))
 entities = list(pd.Series(deaths_df.Entity))
+# Removendo a entidade Mundo da lista
 entities.pop(-4)
 numbers.pop(-4)
-print(entities)
-print(numbers)
+# Criando imagem
 fig = go.Figure(data=go.Choropleth(
     locations=entities,
     z = numbers,
@@ -21,5 +21,6 @@ fig = go.Figure(data=go.Choropleth(
     colorbar_title = "Mortes",
     )
 )
-fig.update_layout(title_text='Mortes em acidentes de transito no mundo em 2017, segundo OMS')
+fig.update_layout(title_text='Mortes em acidentes de transito no mundo em 2017, segundo OMS ((Organização Mundial da Saúde)')
+# Renderizando
 fig.show()
